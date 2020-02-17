b45cf5d69f13168ecf28b4a4c54a41679dfb3686
Toshiyuki Saito <toshiyuki.saito.16@gmail.com>
2020-02-17 16:33:47 +0900

    fix manylinux build

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -5,9 +5,9 @@ PYTHONS=("cp36-cp36m" "cp37-cp37m" "cp38-cp38")
 for PYTHON in ${PYTHONS[@]}; do
     /opt/python/${PYTHON}/bin/pip install --upgrade pip
     /opt/python/${PYTHON}/bin/pip install -U wheel auditwheel
-    /opt/python/${PYTHON}/bin/python setup.py bdist_wheel
+    /opt/python/${PYTHON}/bin/pip wheel . -w /github/workspace/wheelhouse/
 done
 
-for whl in /github/workspace/dist/*.whl; do
+for whl in /github/workspace/wheelhouse/*.whl; do
     auditwheel repair $whl
 done
